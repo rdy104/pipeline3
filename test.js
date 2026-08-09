31ba27ec6179f1378b0afe86fd07f116a446ef5f
yosh <rdy104@gmail.com>
2026-08-08 21:44:29 -0400

    prueba4

--- test.js
+++ test.js
@@ -1,7 +1,3 @@
-console.log('Ejecutando pruebas de TaskNote...');
-const fs = require('fs');
-if (!fs.existsSync('./index.html')) {
-  console.error('FALLO: index.html no encontrado');
-  process.exit(1);
-}
-console.log('Todas las pruebas pasaron');
+console.log('Ejecutando pruebas...');
+process.exit(1);  // Fuerza el fallo
+
